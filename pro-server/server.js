4b88a41609c6b8512c4fbffdb7b0b897b74cc226
Xinxin He <xinxinhe1991@gmail.com>
2017-08-09 01:51:05 -0400

    Modified port and server

--- pro-server/server.js
+++ pro-server/server.js
@@ -4,14 +4,12 @@ var app = express();
 const path = require('path');
 
 const port = 8080;
-app.set('port', port);
 
 app.use(express.static(path.join(__dirname, '../pro-client/dist/')));
 
 app.use(function(req, res) {
   res.sendFile('index.html', {root: path.join(__dirname, '../pro-client/dist/')});
 });
-
 
 
 const http = require('http');
